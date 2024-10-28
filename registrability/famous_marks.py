2d91698992aecbd8a2248493290042c9f61116e6
Vedik <vedik@LAPTOP-QCDHMACF>
2024-10-28 13:30:42 +0530

    Updated code

--- registrability/famous_marks.py
+++ registrability/famous_marks.py
@@ -1,49 +1,71 @@
 import json, random
 from Levenshtein import distance
 
-def get_famous_marks(mark_name: str):
 
-    mark_name = mark_name.lower()
-    mark_name = mark_name.replace("d","b")
-    mark_name = mark_name.replace("p","q")
-    data = json.load(open("../json/trademark_data.json"))
+class FeatureParams:
 
-    distances = []
-    mark_name_list = mark_name.lower().split()
 
-    for mark in mark_name_list:
-        for item in data:
-            item_distance = min(
-                            distance(mark, item["trademark_name"].lower()),
-                            distance(mark,item["trademark_name"].lower().replace("d","b").replace("p","q")),
-                            ) / len(mark)
-            distances.append(
-                    {"mark": mark, "item": item, "distance": item_distance}
+    def get_famous_marks(self,mark_name: str):
+
+        mark_name = mark_name.lower()
+        mark_name = mark_name.replace("d","b")
+        mark_name = mark_name.replace("p","q")
+        data = json.load(open("../json/trademark_data.json"))
+
+        distances = []
+        mark_name_list = mark_name.lower().split()
+
+        for mark in mark_name_list:
+            for item in data:
+                item_distance = min(
+                                distance(mark, item["trademark_name"].lower()),
+                                distance(mark,item["trademark_name"].lower().replace("d","b").replace("p","q")),
+                                ) / len(mark)
+                distances.append(
+                        {"mark": mark, "item": item, "distance": item_distance}
+                    )
+        
+        sorted_distances = sorted(distances, key=lambda x: x["distance"])
+
+        most_similar_trademarks = []
+        zero_count = 0
+        for data in sorted_distances:
+        
+            if data["distance"] < 0.334:
+                if data["distance"] == 0:
+                    zero_count += 1
+                most_similar_trademarks.append(
+                    {
+                        "name": data["item"]["trademark_name"]# .("b", "d").replace("q", "p"),
+                        
+                    }
                 )
-    
-    sorted_distances = sorted(distances, key=lambda x: x["distance"])
 
-    most_similar_trademarks = []
-    zero_count = 0
-    for data in sorted_distances:
+        if len(most_similar_trademarks) == 0:
+            return 1, most_similar_trademarks
+        if zero_count == 0:
+            return 0.5,most_similar_trademarks
+        
+        return 4 / (100* zero_count), most_similar_trademarks
     
-        if data["distance"] < 0.334:
-            if data["distance"] == 0:
-                zero_count += 1
-            most_similar_trademarks.append(
-                {
-                    "name": data["item"]["trademark_name"]# .("b", "d").replace("q", "p"),
-                    
-                }
-            )
+    def get_famous_personality(self,request):
 
-    if len(most_similar_trademarks) == 0:
-        return 1, most_similar_trademarks
-    if zero_count == 0:
-        return 0.5,most_similar_trademarks
-    
-    return 4 / (100* zero_count), most_similar_trademarks
-            
+        data = request.data.get("trademark_name")
+
+        resp_dict = {}
+
+        for i in data.split(" "):
+            resp_dict[i] = KnowledgeGraph(i).run()
+        except Exception as e:
+            return "Wrong"
+        
+        data = 
+
+
+
+
+        
+                
 
 
 
